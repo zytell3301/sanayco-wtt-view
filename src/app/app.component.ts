c31669d57f277ff81e61950d05c8245023364893
zytell3301 <zytel3301@gmail.com>
2022-04-17 14:18:50 +0430

    Implement dashboard navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable({
+  providedIn: 'root',
+})
 export class AppComponent {
   title = 'wtt';
+  private httpClient: HttpClient;
+
+  constructor(client: HttpClient) {
+    this.httpClient = client;
+  }
+
+  StorePresentation() {
+    let headers = new HttpHeaders();
+    headers.set('Access-Control-Allow-Origin', '*');
+    this.httpClient.post("https://127.0.0.1:5001/presentation/record", {user_id: 4}).subscribe(response => {
+      console.log(response);
+      console.log(response.valueOf());
+    });
+  }
+
+  StorePresentationEnd() {
+    this.httpClient.post("https://127.0.0.1:5001/presentation/record-end", {user_id: 4}).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
